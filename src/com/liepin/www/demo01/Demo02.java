04507ef5b6f5c4673cdbd94fcea7986c65fbf904
李刚 <lg@ligangdeMacBook-Pro.local>
2020-12-23 23:13:17 +0800

    20201223

---
+++ src/com/liepin/www/demo01/Demo02.java
@@ -0,0 +1,8 @@
+package com.liepin.www.demo01;
+
+public class Demo02 {
+    public static void main(String[] args){
+        Demp01 demp01 = new Demp01();
+
+    }
+}
---
+++ src/com/liepin/www/demo01/Demp01.java
@@ -0,0 +1,18 @@
+package com.liepin.www.demo01;
+
+public class Demp01<T> {
+
+    public void method1(String str){
+        System.out.println(str);
+    }
+
+    public void method2(Integer integer){
+        System.out.println(integer);
+    }
+
+    public static void main(String[] args){
+        Demp01 demp01 = new Demp01();
+        demp01.method1("Hello");
+        demp01.method2(1);
+    }
+}
