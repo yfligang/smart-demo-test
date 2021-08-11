b79b945bcb4e8d039dddb50d25dbf4bebb672a88
yfligang <yfligang1984@gmail.com>
2021-08-11 23:49:16 +0800

    20210811

--- src/demo14/MergeArray.java
+++ src/demo14/MergeArray.java
@@ -2,4 +2,10 @@ package demo14;
 
 public class MergeArray {
 
+    public static void main(String[] args){
+        StringBuffer sb1 = new StringBuffer(10);
+        StringBuilder sb2 = new StringBuilder(10);
+        boolean bl1 = sb1.equals(sb2);
+        System.out.println(bl1);
+    }
 }
