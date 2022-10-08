521c3014f5c11a0f0efdbb74a40dce219fa7dc7a
yfligang <yfligang1984@gmail.com>
2022-10-08 23:51:10 +0800

    20221008_new

---
+++ src/study/Learn0003.java
@@ -0,0 +1,12 @@
+package study;
+
+public class Learn0003 {
+
+    public static void main(String[] args){
+        Learn0002 learn0002 = new Learn0002();
+        Learn0002.testSplit01();
+        Learn0002.testSplit();
+        Learn0002.testSplitUpdate01();
+        Learn0002.testSplitUpdate02();
+    }
+}
